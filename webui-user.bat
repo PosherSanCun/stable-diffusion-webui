ac0fc5a1de583a6e211023d34b29ec6a48191727
SanCun <50034800+PosherSanCun@users.noreply.github.com>
2023-06-28 04:22:44 +0800

    本地分支

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,17 @@
+
 @echo off
 
+rem 清空PYTHON变量，用于指定Python可执行文件的路径
 set PYTHON=
+
+rem 清空GIT变量，用于指定Git可执行文件的路径
 set GIT=
+
+rem 清空VENV_DIR变量，用于指定venv环境的目录路径
 set VENV_DIR=
+
+rem 清空COMMANDLINE_ARGS变量，用于指定webui.py的命令行参数
 set COMMANDLINE_ARGS=
 
+rem 调用webui.bat脚本
 call webui.bat
